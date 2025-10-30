eda14884969c74ac090cf12489bd5ad2f3858790
LeonardAzah <isongleonard@gmail.com>
2025-10-30 04:48:52 +0100

    add search, authentication and permission to user profile

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -5,6 +5,7 @@ from profiles_api import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename="hello-viewset")
+router.register('profiles', views.UserProfileViewSet)
 
 
 urlpatterns = [
